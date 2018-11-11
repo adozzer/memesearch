39d47fbea30b435cd46963aa85680d3e42bbbbf3
adozzer <adozzera@gmail.com>
2018-11-11 10:14:51 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -95,7 +95,8 @@ function generate_search_result_image_block(image, text) {
 
     // try {
         var xhr = new XMLHttpRequest();
-        var url = "https://searchmeme.ml/predict";
+        const proxyurl = "";
+        var url = proxyurl+"http://searchmeme.ml/predict";
         xhr.open("POST", url, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onreadystatechange = function () {
@@ -257,7 +258,7 @@ class MemeExplorer extends React.Component {
 
         <Group>
           <Header level="2">
-            Puca
+            Удивлённый Пикачу
           </Header>
           <Div align="middle">
             <img width="500" src="https://memepedia.ru/wp-content/uploads/2018/10/4-55-768x768.jpg"></img>
@@ -283,32 +284,15 @@ class MemeExplorer extends React.Component {
                   <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://memepedia.ru/wp-content/uploads/2018/10/meme8-13.jpg"></img>
                 </div>  
                 <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://lh6.googleusercontent.com/S9CJMzspU3ur33HcQkTDcBJMIVwRdLrwTGzqzavt5uhzniql2NM7PRmtcAupZt1p-NOJD5wmzKgziN3hJLPa=w1215-h710"></img>
+                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://i.imgur.com/mqlrxPx.jpg"></img>
                 </div>  
                 <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://lh5.googleusercontent.com/hlpdVFQv5i8x-W7lMZ_rdJ4BLZSp6n1O9hqk8UIh0SFh-7vcO17Lot3mHfTUcOkkETNK1cajuAxAXKERuuWZ=w1215-h710"></img>
+                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://i.imgur.com/WWWNbP6.jpg"></img>
                 </div> 
                  <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://lh5.googleusercontent.com/JmBD_06w5n2KWzlZyl_uC-eDSXmD1Uw4bd9RH5OZtPp_YjUTl4XZZNeOJJSpluTKigCNvxVcSt1x8ftBnZrm=w1215-h710-rw"></img>
+                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://i.imgur.com/tevTDqk.jpg"></img>
                 </div>   
-                <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://memepedia.ru/wp-content/uploads/2018/10/4-55-768x768.jpg"></img>
-                </div>  
-                <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://memepedia.ru/wp-content/uploads/2018/10/5-52.jpg"></img>
-                </div>  
-                <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://memepedia.ru/wp-content/uploads/2018/10/meme8-13.jpg"></img>
-                </div>  
-                <div>
-                  <img style={{ padding: '8px', align: 'middle' }} width="500" src="https://lh6.googleusercontent.com/S9CJMzspU3ur33HcQkTDcBJMIVwRdLrwTGzqzavt5uhzniql2NM7PRmtcAupZt1p-NOJD5wmzKgziN3hJLPa=w1215-h710"></img>
-                </div>  
-                <div>
-                  <img style={{ padding: '8px' }} width="500" src="https://lh5.googleusercontent.com/hlpdVFQv5i8x-W7lMZ_rdJ4BLZSp6n1O9hqk8UIh0SFh-7vcO17Lot3mHfTUcOkkETNK1cajuAxAXKERuuWZ=w1215-h710"></img>
-                </div> 
-                 <div>
-                  <img style={{ padding: '8px' }} width="500" src="https://lh5.googleusercontent.com/JmBD_06w5n2KWzlZyl_uC-eDSXmD1Uw4bd9RH5OZtPp_YjUTl4XZZNeOJJSpluTKigCNvxVcSt1x8ftBnZrm=w1215-h710-rw"></img>
-                </div>      
+                   
             </div>
           </HorizontalScroll>
         </Group>
